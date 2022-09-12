21e8bd6d0d8f09f034726eb7f194965d2583639e
raturidrc <gaurav.raturi@drcsystems.com>
2022-09-12 16:55:47 +0530

    First Commit fron intelij idea PRoJECT

--- src/main/java/MyGit/DevClass.java
+++ src/main/java/MyGit/DevClass.java
@@ -1,4 +1,12 @@
 package MyGit;
 
 public class DevClass   {
+    public static void main(String[] args) {
+        System.out.println("jkshdfjksdhjklf");
+
+    }
+//asdnkajndkajdlkajdlkajd]
+// aldnklajdlkajsd
+// akjshdkahdklashd
+// ]alsdadajkhdkjas]
 }
--- src/main/java/MyGit/MyGit.java
+++ src/main/java/MyGit/MyGit.java
@@ -2,6 +2,6 @@ package MyGit;
 
 public class MyGit {
     public static void main(String[] args) {
-
+System.out.println("aklsjdlkajsdklajl");
     }
 }
