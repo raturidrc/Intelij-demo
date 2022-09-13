376e297d34329e3c42dd3844a17678479c2a0b75
raturidrc <gaurav.raturi@drcsystems.com>
2022-09-13 09:46:42 +0530

    Creating a new Branch

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,6 +1,9 @@
 public class Demo2 {
     public static void main(String[] args) {
 
-        System.out.println("This is demo 2");
+        System.out.println("This is demo 2");]
+        System.out.println("Creating new Branch ");
+        System.out.println("Creating new Branch ");
+
     }
 }
